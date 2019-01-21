a82296f23bba3f4064b1b63fe532d7e6ee92d066
Adam Sah <140002+asah@users.noreply.github.com>
2019-01-21 12:43:05 -0500

    fixes

--- setup.sh
+++ setup.sh
@@ -23,13 +23,14 @@ kubectl apply -f secrets.yaml; kubectl create -f master.yaml; kubectl create -f 
 while [ 1 ]; do w=`kubectl get pods | egrep -c 'citus-worker.+Running'`; if [ $w = $WORKERS ]; then break; fi; echo "$w kubernetes containers running, waiting for $WORKERS... "; sleep 2; done
 while [ 1 ]; do w=`kubectl exec -it $CITUS_MASTER -- su postgres -c "psql -c \"SELECT * FROM master_get_active_worker_nodes(); \"" | grep -c citus-workers`; if [ $w = $WORKERS ]; then break; fi; echo "$w citus workers registered, waiting for $WORKERS... "; sleep 2; done
 
+export CITUS_MASTER=$(kubectl get pod -l app=citus-master -o jsonpath="{.items[0].metadata.name}")
 kubectl exec -it $CITUS_MASTER -- su postgres -c "psql -c \"create database citus;\"" |egrep -v "NOTICE|DETAIL|HINT" # must be its own command...
 kubectl exec -it $CITUS_MASTER -- su postgres -c "psql -c \"SELECT run_command_on_workers('create database citus');\""
 
 # requires createdb citus...
 kubectl exec -it $CITUS_MASTER -- su postgres -c "psql citus -c \"create extension citus\""
-kubectl exec -it $CITUS_MASTER -- su postgres -c "psql citus -c \"SELECT run_command_on_workers('create extension citus'); \"
+kubectl exec -it $CITUS_MASTER -- su postgres -c "psql citus -c \"SELECT run_command_on_workers('create extension citus'); \""
 
 # create test data and run tests
-kubectl exec -it $CITUS_MASTER -- su - postgres -c "psql citus" < citus.sql  # "su -" is important so we can write to local disk
+kubectl exec -it $CITUS_MASTER -- su - postgres -c "psql citus" < citus-test.sql  # "su -" is important so we can write to local disk
 
