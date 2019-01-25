3ca75c2a7a47fbba78a1aba609483ecf0a884e58
Adam Sah <140002+asah@users.noreply.github.com>
2019-01-25 12:15:03 -0500

    split out redeploy, better automate gcloud

--- setup.sh
+++ setup.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
-wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-230.0.0-linux-x86_64.tar.gz
-tar zxf google-cloud-sdk-230.0.0-linux-x86_64.tar.gz google-cloud-sdk
-./google-cloud-sdk/install.sh -q
-source ~/.bashrc
-gcloud components install kubectl -q
-gcloud components update
+if [ "x$GCLOUD_PROJECT" = "x" ]; then
+  echo "please set GCLOUD_PROJECT variable, e.g. mgh-neurology-poc-01"; exit 1
+fi
+if [ "x$GCLOUD_COMPUTE_ZONE" = "x" ]; then
+  echo "please set GCLOUD_COMPUTE_ZONE variable, e.g. us-east1-c"; exit 1
+fi
+if [ "x$GCLOUD_CLUSTER" = "x" ]; then
+  echo "please set GCLOUD_CLUSTER variable to the k8s cluster, e.g. citus-test-asah-01"; exit 1
+fi
+
+if ! [ -x "$(command -v gcloud)" ]; then
+  /bin/rm -fr google-cloud-sdk-230.0.0-linux-x86_64.tar.gz google-cloud-sdk
+  wget https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-sdk-230.0.0-linux-x86_64.tar.gz
+  tar zxf google-cloud-sdk-230.0.0-linux-x86_64.tar.gz google-cloud-sdk
+  ./google-cloud-sdk/install.sh -q
+  source ~/.bashrc
+fi
+
+if ! [ -x "$(command -v kubectl)" ]; then
+  gcloud components install kubectl -q
+fi
+
+gcloud components update -q
 
 # interactive login to your google account...
 gcloud auth login
-gcloud config set project mgh-neurology-poc-01
-gcloud config set compute/zone us-east1-c
-gcloud container clusters get-credentials citus-test-asah-01
+gcloud config set project $GCLOUD_PROJECT
+gcloud config set compute/zone $GCLOUD_COMPUTE_ZONE
+gcloud container clusters get-credentials $GCLOUD_CLUSTER
 
-export WORKERS=7
-kubectl delete deployment.apps/citus-master service/citus-master service/citus-workers; kubectl delete sts citus-worker; kubectl delete pvc citus-master-pvc; echo "waiting for termination..."; sleep 30; kubectl get all
-
-# https://stackoverflow.com/questions/47389443/finding-the-name-of-a-new-pod-with-kubectl
-kubectl apply -f secrets.yaml; kubectl create -f master.yaml; kubectl create -f workers.yaml; echo "waiting..."; sleep 10; kubectl get all; export CITUS_MASTER=$(kubectl get pod -l app=citus-master -o jsonpath="{.items[0].metadata.name}")
-
-# make sure k8s and citus are running
-while [ 1 ]; do w=`kubectl get pods | egrep -c 'citus-worker.+Running'`; if [ $w = $WORKERS ]; then break; fi; echo "$w kubernetes containers running, waiting for $WORKERS... "; sleep 2; done
-while [ 1 ]; do w=`kubectl exec -it $CITUS_MASTER -- su postgres -c "psql -c \"SELECT * FROM master_get_active_worker_nodes(); \"" | grep -c citus-workers`; if [ $w = $WORKERS ]; then break; fi; echo "$w citus workers registered, waiting for $WORKERS... "; sleep 2; done
-
-export CITUS_MASTER=$(kubectl get pod -l app=citus-master -o jsonpath="{.items[0].metadata.name}")
-kubectl exec -it $CITUS_MASTER -- su postgres -c "psql -c \"create database citus;\"" |egrep -v "NOTICE|DETAIL|HINT" # must be its own command...
-kubectl exec -it $CITUS_MASTER -- su postgres -c "psql -c \"SELECT run_command_on_workers('create database citus');\""
-
-# requires createdb citus...
-kubectl exec -it $CITUS_MASTER -- su postgres -c "psql citus -c \"create extension citus\""
-kubectl exec -it $CITUS_MASTER -- su postgres -c "psql citus -c \"SELECT run_command_on_workers('create extension citus'); \""
-
-# create test data and run tests
-kubectl exec -it $CITUS_MASTER -- su - postgres -c "psql citus" < citus-test.sql  # "su -" is important so we can write to local disk
+exec ./redeploy.sh
 
